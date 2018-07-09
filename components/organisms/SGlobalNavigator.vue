4250520adfbdf0e216627962099c805d02cb87f8
Joe-noh <goflb.jh@gmail.com>
2018-07-10 01:19:37 +0900

    define routes and implement global navigator

--- components/organisms/SGlobalNavigator.vue
+++ components/organisms/SGlobalNavigator.vue
@@ -1,5 +1,27 @@
 <template>
-  <nav class="s-global-navigator">nav</nav>
+  <nav class="s-global-navigator">
+    <ul class="list">
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 't-shirt' }}">Tシャツ</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'ringer-t-shirt' }}">リンガーTシャツ</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'long-sleeve-t-shirt' }}">ロングスリーブTシャツ</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'full-graphic-t-shirt' }}">フルグラフィックTシャツ</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'sweat' }}">スウェット</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'hoodie' }}">フーディ</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'mug' }}">マグカップ</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'towel-handkerchief' }}">タオルハンカチ</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'blanket' }}">ブランケット</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'sticker' }}">ステッカー</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'can-badge' }}">缶バッジ</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'note' }}">ノート</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'smartphone-case' }}">スマートフォンケース</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'clear-smartphone-case' }}">クリアスマートフォンケース</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'wallet-phone-case' }}">ウォレットフォンケース</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'tote-bag' }}">トートバッグ</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'sacoche' }}">サコッシュ</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'baby-bib' }}">ベイビービブ</router-link></li>
+      <li class="list-item"><router-link class="list-item__link" :to="{name: 'items-itemName', params: { itemName: 'baby-rompers' }}">ベイビーロンパース</router-link></li>
+    </ul>
+  </nav>
 </template>
 
 <style lang="scss" scoped>
@@ -10,5 +32,26 @@
   width: 100%;
   background-color: $white;
   border-bottom: solid 1px $graylight;
+}
+
+.list {
+  list-style: none;
+  text-decoration: none;
+  display: flex;
+  flex-flow: row nowrap;
+  overflow-x: scroll;
+  padding: 0 5px;
+}
+
+.list-item {
+  line-height: 40px;
+  display: table-cell;
+  white-space: nowrap;
+  margin: 0 5px;
+}
+
+.list-item__link {
+  text-decoration: none;
+  font-size: 14px;
 }
 </style>
---
+++ pages/items/_itemName/index.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <div>{{ itemId }}</div>
+</template>
+
+<script>
+const items = {
+  't-shirt': 1,
+  'ringer-t-shirt': 19,
+  'long-sleeve-t-shirt': 15,
+  'full-graphic-t-shirt': 8,
+  'sweat': 5,
+  'hoodie': 9,
+  'mug': 3,
+  'towel-handkerchief': 14,
+  'blanket': 13,
+  'sticker': 11,
+  'can-badge': 17,
+  'note': 10,
+  'smartphone-case': 4,
+  'clear-smartphone-case': 18,
+  'wallet-phone-case': 12,
+  'tote-bag': 2,
+  'sacoche': 16,
+  'baby-rompers': 6,
+  'baby-bib': 7,
+}
+
+export default {
+  computed: {
+    itemId() {
+      return items[this.$route.params.itemName]
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
