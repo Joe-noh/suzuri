f2dc9daf2027b98f14aa74860854a514e92f912b
Joe-noh <goflb.jh@gmail.com>
2018-07-09 21:29:34 +0900

    list surisuri pickups

---
+++ components/organisms/SPickupList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="s-pickup-list">
+    <div v-for="pickup in pickups">
+      {{ pickup.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pickups: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,61 +1,36 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        suzuri
-      </h1>
-      <h2 class="subtitle">
-        Kage-bunshin no Jitsu
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <s-pickup-list :pickups="pickups"/>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import SPickupList from '~/components/organisms/SPickupList.vue'
 import axios from 'axios'
 
 export default {
   components: {
-    Logo,
+    SPickupList,
+  },
+  data () {
+    return {
+      pickups: [],
+    }
   },
   async mounted() {
-    const res = await axios.get('/api/v1/activities?hoge=fuga')
-    console.log(res)
+    this.pickups = await this.fetchPickups()
+    console.log(this.pickups);
+  },
+  methods: {
+    async fetchPickups() {
+      const params = { userName: 'surisurikun', limit: 4 }
+      const res = await axios.get('/api/v1/choices', { params })
+
+      return res.data.choices
+    },
   },
 }
 </script>
 
-<style lang="scss">
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-    'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links {
-  padding-top: 15px;
-}
+<style lang="scss" scoped>
 </style>
