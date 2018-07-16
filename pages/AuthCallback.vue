cc8cd002365019735d793c9c7714646a1b34792f
Joe-noh <goflb.jh@gmail.com>
2018-07-17 00:22:26 +0900

    callback page

---
+++ pages/AuthCallback.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="a">
+    callback
+  </div>
+</template>
+
+<script>
+import { fetchAccessToken, loginAsSuzuriUser } from '~/lib/suzuri'
+
+export default {
+  props: {
+    code: {
+      type: String,
+      required: true,
+    },
+  },
+  async mounted() {
+    const accessToken = await fetchAccessToken(this.code)
+    const user = await loginAsSuzuriUser(accessToken)
+  },
+}
+</script>
