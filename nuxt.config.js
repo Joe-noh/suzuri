03050b4a5b1c1a4d8631df567f64eaa9fd334091
Joe-noh <goflb.jh@gmail.com>
2018-07-16 14:18:48 +0900

    name and des

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Cache and Cache.' },
+      { hid: 'description', name: 'description', content: 'Ninja the Quick' },
     ],
   },
 
