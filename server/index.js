c7472e2d72f9d5f049e06999fd1849055e084d99
Joe-noh <goflb.jh@gmail.com>
2018-07-18 03:00:13 +0900

    exchange code with access token

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const proxy = require('express-http-proxy')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
+const bodyParser = require('body-parser')
 const authRoute = require('./routes/auth')
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3001
@@ -13,15 +14,14 @@ let config = require('../nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
 
 async function start() {
-  // Init Nuxt.js
   const nuxt = new Nuxt(config)
 
-  // Build only in dev mode
   if (config.dev) {
     const builder = new Builder(nuxt)
     await builder.build()
   }
 
+  app.use(bodyParser.json())
   app.use('/auth', authRoute)
 
   app.use(
@@ -37,11 +37,10 @@ async function start() {
     }),
   )
 
-  // Give nuxt middleware to express
   app.use(nuxt.render)
 
-  // Listen the server
   app.listen(port, host)
   console.log('Server listening on http://' + host + ':' + port) // eslint-disable-line no-console
 }
+
 start()
