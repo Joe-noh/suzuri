7a6a6be931c97edb2d6c481cb675cd50ae66bccc
Joe-noh <goflb.jh@gmail.com>
2018-07-20 01:25:55 +0900

    move logic to model

--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,5 @@
+const axios = require('axios')
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     suzuriId: {
@@ -20,6 +22,40 @@ module.exports = (sequelize, DataTypes) => {
 
   User.associate = (models) => {
     // associations can be defined here
+  }
+
+  User.exchangeTokenWithCode = async (code) => {
+    const params = new URLSearchParams()
+    params.append('grant_type', 'authorization_code')
+    params.append('code', code)
+    params.append('redirect_uri', process.env.SUZURI_REDIRECT_URI)
+    params.append('client_id', process.env.SUZURI_CLIENT_ID)
+    params.append('client_secret', process.env.SUZURI_CLIENT_SECRET)
+
+    const res = await axios.post('https://suzuri.jp/oauth/token', params)
+    return accessToken = res.data.access_token
+  }
+
+  User.registerOrLogin = async (token) => {
+    const res = await axios.get('https://suzuri.jp/api/v1/user', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+    const suzuriUser = res.data.user
+
+    return User.findOrCreate({
+      where: {
+        suzuriId: suzuriUser.id.toString(),
+      },
+      defaults: {
+        name: suzuriUser.name,
+        avatarUrl: suzuriUser.avatarUrl,
+        accessToken,
+      }
+    }).spread((user, isCreated) => {
+      return user
+    })
   }
 
   return User
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,41 +11,16 @@ router.use(bodyParser.json())
 router.post('/login', async (req, res, next) => {
   const code = req.body.code
 
-  const params = new URLSearchParams()
-  params.append('grant_type', 'authorization_code')
-  params.append('code', code)
-  params.append('redirect_uri', process.env.SUZURI_REDIRECT_URI)
-  params.append('client_id', process.env.SUZURI_CLIENT_ID)
-  params.append('client_secret', process.env.SUZURI_CLIENT_SECRET)
-
   try {
-    const tokenResponse = await axios.post('https://suzuri.jp/oauth/token', params)
-    const accessToken = tokenResponse.data.access_token
+    const token = await User.exchangeTokenWithCode(code)
+    const user = await User.registerOrLogin(token)
 
-    const userResponse = await axios.get('https://suzuri.jp/api/v1/user', {
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    })
-
-    const suzuriUser = userResponse.data.user
-
-    await User.findOrCreate({
-      where: { suzuriId: suzuriUser.id.toString() },
-      defaults: {
-        name: suzuriUser.name,
-        avatarUrl: suzuriUser.avatarUrl,
-        accessToken,
-      }
-    }).spread((user, isCreated) => {
-      console.log(user)
-
-      return res.json({
-        name: user.name,
-        avatarUrl: user.avatarUrl,
-      })
+    return res.json({
+      name: user.name,
+      avatarUrl: user.avatarUrl,
     })
   } catch (e) {
+    console.log(e);
     res.status(401).json({ error: e.response.data })
   }
 })
