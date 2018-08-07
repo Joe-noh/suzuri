4213c3de6397d85a9cf19a422b9b3f80f7b4218d
Joe-noh <goflb.jh@gmail.com>
2018-08-07 21:12:49 +0900

    replace hard tab with soft

--- webpack/server.config.js
+++ webpack/server.config.js
@@ -3,33 +3,33 @@ const config = require('sapper/webpack/config.js');
 const pkg = require('../package.json');
 
 module.exports = {
-	entry: config.server.entry(),
-	output: config.server.output(),
-	target: 'node',
-	node: {
-	  __dirname: true,
-	},
-	resolve: {
-		extensions: ['.js', '.json', '.html'],
-		mainFields: ['svelte', 'module', 'browser', 'main']
-	},
-	externals: Object.keys(pkg.dependencies),
-	module: {
-		rules: [
-			{
-				test: /\.html$/,
-				use: {
-					loader: 'svelte-loader',
-					options: {
-						css: false,
-						generate: 'ssr'
-					}
-				}
-			}
-		]
-	},
-	mode: process.env.NODE_ENV,
-	performance: {
-		hints: false // it doesn't matter if server.js is large
-	}
+  entry: config.server.entry(),
+  output: config.server.output(),
+  target: 'node',
+  node: {
+    __dirname: true,
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.html'],
+    mainFields: ['svelte', 'module', 'browser', 'main']
+  },
+  externals: Object.keys(pkg.dependencies),
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'svelte-loader',
+          options: {
+            css: false,
+            generate: 'ssr'
+          }
+        }
+      }
+    ]
+  },
+  mode: process.env.NODE_ENV,
+  performance: {
+    hints: false // it doesn't matter if server.js is large
+  }
 };
